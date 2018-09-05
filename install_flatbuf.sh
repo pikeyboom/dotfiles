91e5acfed4d5ddb48cff0b03cde2b653304ab07a
dpike <david.pike>
2018-09-05 22:26:19 +0000

    Added script to install flatbuffers

---
+++ install_flatbuf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+OLD_PWD=$(pwd)
+cd /tmp
+curl -OL https://github.com/google/flatbuffers/archive/v1.5.0.zip
+unzip v1.5.0.zip
+cd flatbuffers-1.5.0
+mkdir build && cd build
+cmake ..
+make && sudo make install
+cd $OLD_PWD
